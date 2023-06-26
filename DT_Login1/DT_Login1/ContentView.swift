5e9d418a10a86171ff8daee2eeb3463e1f86bb79
Devang Tandel <devang.tandel@heliossolutions.co>
2023-06-26 14:52:05 +0530

    first

---
+++ DT_Login1/DT_Login1/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  DT_Login1
+//
+//  Created by Devang Helios on 26/06/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DT_Login1/DT_Login1/DT_Login1App.swift
@@ -0,0 +1,17 @@
+//
+//  DT_Login1App.swift
+//  DT_Login1
+//
+//  Created by Devang Helios on 26/06/23.
+//
+
+import SwiftUI
+
+@main
+struct DT_Login1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
